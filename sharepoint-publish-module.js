c1c73cec75b70a162c6c0758daed624307329659
Victor Alencar <victor.santos@iteris.com.br>
2018-11-06 18:32:46 -0200

    Alterações no module

--- sharepoint-publish-module.js
+++ sharepoint-publish-module.js
@@ -7,29 +7,34 @@ var args = process.argv.slice(2);
 
 const run = async () => {
 
-  //Solicitando credenciais do Sharepoint via linha de comando.
-  const credentials = await inquirer.askSharepointCredentials();
+    //Solicitando credenciais do Sharepoint via linha de comando.
+    //const credentials = await inquirer.askSharepointCredentials();
 
-  if (args && args.length) {
+    if (args && args.length) {
 
-    var environment = Object.keys(environments).filter(value => value === args[0].replace(/-/g, ""));
-    if (environment) {
-      environment  = environments[environment];
+        var environment = Object.keys(environments).filter(value => value === args[0].replace(/-/g, ""));
+        if (environment) {
+            environment = environments[environment];
 
-    spsave(environment.coreOpts, credentials, environment.fileOpts)
-      .then(function (data) {
-        var dateNow = new Date();
-        var dateString = util.format('[%s:%s:%s]', ('0' + dateNow.getHours()).slice(-2), ('0' + dateNow.getMinutes()).slice(-2), ('0' + dateNow.getSeconds()).slice(-2));
-        console.log(dateString + " O upload dos arquivos foi feito com êxito");
-      })
-      .catch(function (err) {
-        var dateNow = new Date();
-        var dateString = util.format('[%s:%s:%s]', ('0' + dateNow.getHours()).slice(-2), ('0' + dateNow.getMinutes()).slice(-2), ('0' + dateNow.getSeconds()).slice(-2));
-        console.log(dateString + " Erro ao fazer upload para a biblioteca " + environment.fileOpts.folder);
-        console.log(dateString + " Mensagem de erro: " + err.message);
-      });
+            spsave(environment.coreOpts, credentials, environment.fileOpts)
+                .then(function(data) {
+                    var dateNow = new Date();
+                    var dateString = util.format('[%s:%s:%s]', ('0' + dateNow.getHours()).slice(-2), ('0' + dateNow.getMinutes()).slice(-2), ('0' + dateNow.getSeconds()).slice(-2));
+                    console.log(dateString + " O upload dos arquivos foi feito com êxito");
+                })
+                .catch(function(err) {
+                    var dateNow = new Date();
+                    var dateString = util.format('[%s:%s:%s]', ('0' + dateNow.getHours()).slice(-2), ('0' + dateNow.getMinutes()).slice(-2), ('0' + dateNow.getSeconds()).slice(-2));
+                    console.log(dateString + " Erro ao fazer upload para a biblioteca " + environment.fileOpts.folder);
+                    console.log(dateString + " Mensagem de erro: " + err.message);
+                });
+        }
+    } else {
+      var dateNow = new Date();
+      var dateString = util.format('[%s:%s:%s]', ('0' + dateNow.getHours()).slice(-2), ('0' + dateNow.getMinutes()).slice(-2), ('0' + dateNow.getSeconds()).slice(-2));
+      console.log(dateString + " Erro ao publicar arquivos");
+      console.log(dateString + " Mensagem de erro: " + err.message);
     }
-  }
 }
 
 run();
